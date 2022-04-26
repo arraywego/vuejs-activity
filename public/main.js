3a1c00e5db0b087745c8c1df22aedd2170c9d835
arraywego <elara5@uci.edu>
2022-04-26 15:20:33 -0700

    Reset files to remove all activities

--- public/main.js
+++ public/main.js
@@ -1,14 +1,2 @@
-const app =Vue.createApp ({
-    data (){
-        return{
-            product: 'Socks',
-            image: 'images/socks_green.jpg',
-            // solution
-            url: 'https://www.vuemastery.com/',
-            // solution
 
-            inStock: true
-        }
-    }
-})
 
